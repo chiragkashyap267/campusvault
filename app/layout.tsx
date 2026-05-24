df0351e785e584316f08203bb8fd7e33b7598498
chirag kashyap <businesswithchirag267@gmail.com>
2026-05-25 00:07:30 +0530

    added new files

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { SmoothScroll } from "@/components/layout/SmoothScroll";
 import { RobotAssistant } from "@/components/shared/RobotAssistant";
+import Script from "next/script";
 import { SITE_NAME, SITE_DESCRIPTION } from "@/lib/constants";
 
 const inter = Inter({ 
@@ -67,6 +68,26 @@ export default function RootLayout({
           <Providers>{children}</Providers>
           <RobotAssistant />
         </SmoothScroll>
+
+        {/* Google Analytics Loader */}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
